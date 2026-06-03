6e5a95c4a9540016b90cf89d9acf664ee7691f2a
Mikołaj Kadłubowski <mikolaj_kadlubowski@wp.pl>
2026-06-03 18:01:54 +0200

    Reworked loader

--- data_loader.py
+++ data_loader.py
@@ -1,15 +1,18 @@
 import yfinance
-import pandas as pd
 
-def load_data(ticker="NVDA", start_date="2023-01-01", end_date="2026-01-01"):
-    """
-    Loads historical stock data for a given ticker.
-    """
-    data = yfinance.download(ticker, period = '3y', interval = '1d')["Close"]
-    data.dropna(inplace=True)
-    data.reset_index(inplace=True)
-    data['Date'] = pd.to_datetime(data['Date']) # Ensure 'Date' column is datetime objects
-    data.set_index('Date', inplace=True) # Set 'Date' as the index
-    data.sort_index(inplace=True) # Sort data by date
+# Loading historical data
+def load_data(ticker="NVDA"):
 
-    return data
+    data = yfinance.download(ticker, period='3y', interval='1d')
+    
+    # Extracting 'Close' price
+    df = data['Close'].copy()
+    df.columns = ['Price']
+    
+    # Cleaning up the data
+    df.dropna(inplace=True)
+    df.sort_index(inplace=True) 
+
+    print(f"Downloaded {ticker} data from {df.index.min()} to {df.index.max()}")
+    return df
+
